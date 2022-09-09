9674fe390d6f47d7c8afb70530f947c05a5a16f7
Iyamokuma <virtuehorsefall@gmail.com>
2022-09-09 03:30:57 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,27 @@
-#include <stdio.h>
+#!/bin/bash
 
-/**
-* main - put the string in the put function
-*
-* Description: using the main function 
-* This program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
-*int main(void)
-{
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
-}
+                                                                                                                                        
+
+/**                                                                                                                                     
+
+* main - print the string in the put function                                                                                           
+
+*                                                                                                                                       
+
+* Description: using the main function                                                                                                  
+
+* This program prints "Programming is like building a multilingual puzzle                                                               
+
+* Return: 0                                                                                                                             
+
+*/                                                                                                                                      
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+            puts("\"Programming is like building a multilingual puzzle");                                                               
+
+            return (0);                                                                                                                 
+
+}                       
