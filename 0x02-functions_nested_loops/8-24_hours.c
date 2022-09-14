74f2e61d4daf112f12657083041e40d634449c8f
Iyamokuma <virtuehorsefall@gmail.com>
2022-09-14 08:42:01 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* jack_bauer - prints every minute of the day of jack bauer
+* jack_baver - prints every minute of the day of jack baver
 * starting from 00:00 to 23:59
 */
-void jack_bauer(void)
+void jack_baver(void)
 {
          int i, j;
 
