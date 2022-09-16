413c6387f53b8f166ecf504953511bdaff608768
Iyamokuma <virtuehorsefall@gmail.com>
2022-09-15 20:14:24 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,30 +7,23 @@
  * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
 */
-int main(void)
-{
-int i;
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf(" Fizz");
-} 
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf(" Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" Fizz_Buzz");
-} else if (i == 1)
-{
-printf("%d", i);
-} else
-{
-printf(" %d", i);
-}
-}
-printf("\n");
-return (0);
-}
+int main()
+            {
+	// output the fizz buzz sequence from 1 to 100
+	// for (int i = 1; i <= 100; i++)
+	// {
+	// // output Fizz Buzz if the number is divisible by 3 AND 5
+	// if ( (i % 3 == 0) && (i % 5 == 0) )
+	// printf("Fizz Buzz\n");    
+	// // output Fizz if the number is divisible by 3
+	// else if (i % 3 == 0)
+	// printf("Fizz\n");
+	// // output Buzz if the number is divisible by 5
+	// else if (i % 5 == 0)
+	// printf("Buzz\n");
+	// // otherwise output the number
+	// else
+	// printf("%d\n", i);
+	// }  
+	// return 0
+	// }
